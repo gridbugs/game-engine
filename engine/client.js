528f1e96d11ce37292cb1652877b6b155ad884d1
Stephen Sherratt <steve@stevebob.net>
2014-11-03 13:24:16 +1100

    added more regions for testing

--- engine/client.js
+++ engine/client.js
@@ -35,7 +35,8 @@ $(function() {
         [[100, 100], [100, 300]],
         [[100, 300], [400, 300]],
         [[550, 300], [600, 300]],
-        [[600, 300], [600, 100]],
+        [[600, 300], [600, 250]],
+        [[600, 150], [600, 100]],
         [[600, 100], [100, 100]]
     ]);
     var room2 = new Region([
@@ -54,13 +55,43 @@ $(function() {
         [[500, 50], [500, 250]],
         [[500, 250], [300, 250]]
     ]);
+    
+    var room4 = new Region([
+        [[600, 150], [600, 50]],
+        [[600, 50], [1200, 50]],
+        [[1200, 50], [1200, 400]],
+        [[1200, 400], [1600, 400]],
+        [[1600, 400], [1600, 600]],
+        [[1600, 600], [800, 600]],
+        [[700, 600], [600, 600]],
+        [[600, 600], [600, 250]]
+    ]);
+
+    var room5 = new Region([
+        [[700, 600], [700, 750]],
+        [[800, 600], [800, 750]]
+    ]);
+
+    var room6 = new Region([
+        [[700, 750], [400, 750]],
+        [[400, 750], [400, 1000]],
+        [[400, 1000], [1000, 1000]],
+        [[1000, 1000], [1000, 750]],
+        [[1000, 750], [800, 750]]
+    ]);
 
     room2.connect(room1, [[400, 300], [550, 300]]);
     room2.connect(room3, [[150, 350], [300, 350]]);
+    room4.connect(room1, [[600, 250], [600, 150]]);
+    room5.connect(room4, [[700, 600], [800, 600]]);
+    room6.connect(room5, [[700, 750], [800, 750]]);
 
     room1.create_collision_processor(agent.rad);
     room2.create_collision_processor(agent.rad);
     room3.create_collision_processor(agent.rad);
+    room4.create_collision_processor(agent.rad);
+    room5.create_collision_processor(agent.rad);
+    room6.create_collision_processor(agent.rad);
 
     agent.enter_region(room1);
     
@@ -99,8 +130,12 @@ $(function() {
         var walls1 = drawer.group(room1.segs.map(function(s){return drawer.line_segment(s[0], s[1], 1)}));
         var walls2 = drawer.group(room2.segs.map(function(s){return drawer.line_segment(s[0], s[1], 1)}));
         var walls3 = drawer.group(room3.segs.map(function(s){return drawer.line_segment(s[0], s[1], 1)})).hide();
+        var walls4 = drawer.group(room4.segs.map(function(s){return drawer.line_segment(s[0], s[1], 1)}));
+        var walls5 = drawer.group(room5.segs.map(function(s){return drawer.line_segment(s[0], s[1], 1)}));
+        var walls6 = drawer.group(room6.segs.map(function(s){return drawer.line_segment(s[0], s[1], 1)})).hide();
 
-        room2.add_display_detector(walls3, walls1, [[350, 400], [350, 550]]);
+        room2.add_display_detector(walls3, [walls1, walls4, walls5], [[350, 400], [350, 550]]);
+        room5.add_display_detector(walls6, [walls1, walls4, walls2], [[700, 675], [800, 675]]);
 
         var filterer = drawer.filter_pipeline([0, 0], [canvas.width, canvas.height]).set_filters();
         
@@ -136,17 +171,20 @@ $(function() {
             drawer.save();
             drawer.translate(agent.pos).rotate(agent.facing+Math.PI/2);
             
-            circle.outline();
+            circle.draw();
             drawer.draw_point(agent.pos, tc('black'), 4);
             agent.border_detect();
             agent.display_detect();
 
 
-            demo.draw();
+            //demo.draw();
             drawer.restore();
             walls1.draw();
             walls2.draw();
             walls3.draw();
+            walls4.draw();
+            walls5.draw();
+            walls6.draw();
             
             filterer.draw();
             
